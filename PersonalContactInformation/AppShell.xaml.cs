39a67a166b869335aa5674f122d720917565a7a5
LucaMichaelis <LucaMichaelis@LUCAM>
2023-08-31 10:14:10 +0200

    Add project files.

---
+++ PersonalContactInformation/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PersonalContactInformation
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
