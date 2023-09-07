985c5afcfde402d8b819b0da5e569f40c2bdebbd
Luca Michaelis <Luca.Michaelis@thinfabrics.com>
2023-09-07 13:09:00 +0200

    Added experimental class Telefonnummer and wrote some notes as a thinking aid

---
+++ PersonalContactInformation.Library/Models/Telefonnummer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalContactInformation.Library.Models
+{
+    internal class Telefonnummer
+    {
+        public int Id { get; set; }
+        public int PersonId { get; set; }
+        public string Nummer { get; set; }
+    }
+}
